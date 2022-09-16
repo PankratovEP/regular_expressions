1470c0e1c6d3f53cafa53079d5642183dfeb65b8
PankratovEP <enkwi@mail.ru>
2022-09-16 16:54:03 +0300

    функция ищущая отдельно слово в строке

--- reg_expr.py
+++ reg_expr.py
@@ -1,8 +1,16 @@
 import sys
 from re import *
 
+
 def poisk_vhozhdeniya():
     for line in sys.stdin:
         line = line.rstrip()
         if len(findall('cat', line)) > 1:
+            print(line)
+
+
+def vhozhdenie_slova():
+    for line in sys.stdin:
+        line = line.rstrip()
+        if search(r'\bcat\b', line):
             print(line)
